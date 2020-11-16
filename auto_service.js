dc076b017900d096ffab1b45be19f92baf431ee1
Pachara966 <pachara.eng@gmail.com>
2020-11-16 20:48:18 +0700

    Add feed moduld

--- auto_service.js
+++ auto_service.js
@@ -13,21 +13,21 @@ const bree = new Bree({
   // logger: new Cabin(),
   // Bree work in UTC time
   jobs: [
-    // {
-    //   name: 'getFarm',
-    //   interval: '60m',
-    // },
-    // {
-    //   name: 'update_timeline_status',
-    //   interval: 'at 5:00 am also at 5:00 pm ',
-    // },
-    // {
-    //   name: 'changeFarmActivate',
-    //   interval: 'at 5:30 am also at 5:30 pm ',
-    // },
     {
-      name: 'testGenerateFeed',
+      name: 'getFarm',
+      interval: '60m',
     },
+    {
+      name: 'update_timeline_status',
+      interval: 'at 5:00 am also at 5:00 pm ',
+    },
+    {
+      name: 'changeFarmActivate',
+      interval: 'at 5:30 am also at 5:30 pm ',
+    },
+    // {
+    //   name: 'testGenerateFeed',
+    // },
     // {
     //   name: 'test2',
     //   worker: {
